cea63d99267ede89fa3546ac2bdc4509553bdd4d
Jim Mollmann <jim.mollmann@gmail.com>
2020-12-05 16:41:11 -0500

    all in a 3 level dictionary '{ { { [] } } }'

--- findDups.py
+++ findDups.py
@@ -36,35 +36,47 @@ for topList in topsList:
                 if name[:1] == '0':
                     continue
                 if os.path.isfile(filename):
-                    print(debugCnt, ': ', size, dev, inode, filename)
+                    #print(debugCnt, ': ', size, dev, inode, filename)
                     if size not in sizes:
-                        sizes[size] = []
+                        sizes[size] = {}
                     if inode not in sizes[size]:
-                        sizes[size].append(inode)
-                    if inode not in inodes:
-                        inodes[inode] = {}
-                        inodes[inode]['links'] = hardlinks
-                        inodes[inode]['names'] = []
-                    inodes[inode]['names'].append(filename)
+                        sizes[size][inode] = {}
+                        sizes[size][inode]['links'] = hardlinks
+                        sizes[size][inode]['names'] = []
+                    sizes[size][inode]['names'].append(filename)
                 else:
                     print('Not a file:', filename)
-
+    
     for size in sizes:
-        print(size)
+        #print(size)
         for inode in sizes[size]:
-            print(inode, len(inodes[inode]['names']))
-            if len(inodes[inode]['names']) == 1:
-                inodes[inode]['md5sum'] = False
+            #print(inode, len(sizes[size][inode]['names']))
+            if len(sizes[size][inode]['names']) == 1:
+                size[size][inode]['md5sum'] = False
             else:
-                file = inodes[inode]['names'][0]
+                file = sizes[size][inode]['names'][0]
                 with open(file, 'rb') as file_to_check:
                     # read contents of the file
                     data = file_to_check.read()    
                     # pipe contents of the file through
                     md5 = hashlib.md5(data).hexdigest()
-                inodes[inode]['md5sum'] = md5
+                sizes[size][inode]['md5sum'] = md5
     
     pp.pprint(sizes)
-    print('\n----------------------------------------------------------\n')
-    pp.pprint(inodes) 
-    
+    for size in sizes:
+        maxV = -1
+        maxK = None
+        for inode in sizes[size]:
+            if sizes[size][inode]['links'] > maxV:
+                maxV = sizes[size][inode]['links']
+                maxK = inode
+        basefile = sizes[size][maxK]['names'][0]
+        #print(size, maxK, maxV, basefile)
+        for inode in sizes[size]:
+            if inode == maxK:
+                continue
+            if sizes[size][maxK]['md5sum'] == sizes[size][inode]['md5sum']:
+                for name in sizes[size][inode]['names']:
+                    print('ln ', basefile, ' ', name)
+                    #os.link(basefile, name)
+                
